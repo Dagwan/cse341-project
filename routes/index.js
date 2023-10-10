83aa8ff65f4f2d1f2b2003915ecc35061a1283aa
dagwan <dagwanpan@gmail.com>
2023-10-10 19:57:23 +0100

    implementing the routes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.use('/', require('./swagger'));
 
 // Include the routes
-router.use('/cse341-project', require('./taskRoute'));
+router.use('/', require('./taskRoute'));
 
 module.exports = router;
